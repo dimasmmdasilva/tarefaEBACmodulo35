da2564de477d34bc3a71621aa20d0bed99eda080
Dimas_Marcondes <dimasmarcondes88@gmail.com>
2024-03-01 13:12:50 -0300

    atualizando projeto_6 EBAC

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,9 @@ import carrinho from '../../assets/images/carrinho.svg'
 const Header = () => (
   <HeaderBar>
     <div>
-      <img src={logo} alt="Eplay" />
+      <Link to="/">
+        <img src={logo} alt="Eplay" />
+      </Link>
       <nav>
         <Links>
           <LinkItem>
